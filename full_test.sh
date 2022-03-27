a3d05a1c5c503a0351df16a3e2d1951fb951ff45
Andrey <ya.andreysavo@gmail.com>
2022-03-27 17:27:09 +0300

    fix

--- full_test.sh
+++ full_test.sh
@@ -3,7 +3,7 @@
 mvn clean install && \
 mvn exec:java -Dexec.mainClass="lang.Main" -Dexec.args="-i ./project9"
 llc -filetype=obj --relocation-model=pic out.ll
-cmake --configure ./CMakeLists.txt
+cmake ./CMakeLists.txt
 cmake --build ./ --target clean
 cmake --build ./ --target compiler_target
 ./compiler_target
