6c6c37c8642d292dfd8019972d5af67cea5be482
Andrey <ya.andreysavo@gmail.com>
2022-03-27 17:15:20 +0300

    fix

---
+++ CMakeFiles/compiler.dir/cmake_clean.cmake
@@ -0,0 +1,28 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/compiler.dir/lib/default.c.o"
+  "CMakeFiles/compiler.dir/lib/locks/atomics.c.o"
+  "CMakeFiles/compiler.dir/lib/locks/spinlock.c.o"
+  "CMakeFiles/compiler.dir/lib/locks/wait_group.c.o"
+  "CMakeFiles/compiler.dir/lib/root_lib.c.o"
+  "CMakeFiles/compiler.dir/lib/scheduler/context.c.o"
+  "CMakeFiles/compiler.dir/lib/scheduler/coroutine.c.o"
+  "CMakeFiles/compiler.dir/lib/scheduler/fiber.c.o"
+  "CMakeFiles/compiler.dir/lib/scheduler/local_queues_with_steal_scheduler.c.o"
+  "CMakeFiles/compiler.dir/lib/scheduler/manager.c.o"
+  "CMakeFiles/compiler.dir/lib/scheduler/scheduler.c.o"
+  "CMakeFiles/compiler.dir/lib/scheduler/switch_context.S.o"
+  "CMakeFiles/compiler.dir/lib/structures/fibonacci_heap.c.o"
+  "CMakeFiles/compiler.dir/lib/structures/hash_map.c.o"
+  "CMakeFiles/compiler.dir/lib/structures/lf_stack.c.o"
+  "CMakeFiles/compiler.dir/lib/structures/list.c.o"
+  "CMakeFiles/compiler.dir/lib/structures/rb_tree.c.o"
+  "CMakeFiles/compiler.dir/lib/structures/splay_tree.c.o"
+  "CMakeFiles/compiler.dir/lib/structures/thin_heap.c.o"
+  "compiler"
+  "compiler.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/compiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- full_test.sh
+++ full_test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ./compile.sh
-cmake ./
-cmake --build ./cmake-build-debug --target compiler
-./cmake-build-debug/compiler
+cmake --configure ./CMakeLists.txt
+cmake --build ./ --target clean
+cmake --build ./ --target compiler
+./compiler
